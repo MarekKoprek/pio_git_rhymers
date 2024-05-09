2ebc37756df19914b71e81c8b1b8a369d4e8ba0c
romanbeznosenko <245125@edu.p.lodz.pl>
2024-05-09 10:54:46 +0200

    6.1. Dodałem stałe dla klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	private static final int SIZE = 12;
 
-	public int total = -1;
+	private static final int CHECK = -1;
+
+	private int[] numbers = new int[SIZE];
+
+	public int total = CHECK;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -12,7 +16,7 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == CHECK;
 	}
 
 	public boolean isFull() {
@@ -21,13 +25,13 @@ public class DefaultCountingOutRhymer {
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return CHECK;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return CHECK;
 		return numbers[total--];
 	}
 
